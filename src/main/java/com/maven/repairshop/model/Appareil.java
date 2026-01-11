3c95254c48744a44fb5e3fd081db99546713c529
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-11 20:26:20 +0100

    create java/*/model/Appareil.java v1

---
+++ src/main/java/com/maven/repairshop/model/Appareil.java
@@ -0,0 +1,32 @@
+package com.maven.repairshop.model;
+
+import com.maven.repairshop.model.base.BaseEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="appareils")
+public class Appareil extends BaseEntity {
+
+    private String imei;
+
+    @Column(nullable=false)
+    private String typeAppareil; // téléphone, PC...
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name="reparation_id", nullable=false)
+    private Reparation reparation;
+
+    public String getImei() { return imei; }
+    public void setImei(String imei) { this.imei = imei; }
+
+    public String getTypeAppareil() { return typeAppareil; }
+    public void setTypeAppareil(String typeAppareil) { this.typeAppareil = typeAppareil; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Reparation getReparation() { return reparation; }
+    public void setReparation(Reparation reparation) { this.reparation = reparation; }
+}
