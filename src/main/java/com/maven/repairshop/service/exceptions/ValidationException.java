23365e74d02e42a5e5c90cbe576fc5961385051f
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-11 20:38:20 +0100

    create java/*/service/exceptions/ValidationException.java v1

---
+++ src/main/java/com/maven/repairshop/service/exceptions/ValidationException.java
@@ -0,0 +1,5 @@
+package com.maven.repairshop.service.exceptions;
+
+public class ValidationException extends BusinessException {
+    public ValidationException(String message) { super(message); }
+}
