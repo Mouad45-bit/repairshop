d93bbd1739068aef368e99ad7cb84f4bf7c590ad
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-12 23:51:58 +0100

    update java/*/ui/util/UiServices.java v3

--- src/main/java/com/maven/repairshop/ui/util/UiServices.java
+++ src/main/java/com/maven/repairshop/ui/util/UiServices.java
@@ -1,8 +1,10 @@
 package com.maven.repairshop.ui.util;
 
 import com.maven.repairshop.service.ClientService;
+import com.maven.repairshop.service.EmpruntService;
 import com.maven.repairshop.service.ReparationService;
 import com.maven.repairshop.ui.mocks.InMemoryClientService;
+import com.maven.repairshop.ui.mocks.InMemoryEmpruntService;
 import com.maven.repairshop.ui.mocks.InMemoryReparationService;
 
 /**
@@ -16,11 +18,13 @@ public final class UiServices {
 
     private ReparationService reparationService;
     private ClientService clientService;
+    private EmpruntService empruntService;
 
     private UiServices() {
         // UI-only : pas de HibernateUtil, pas de DAO ici
         this.reparationService = new InMemoryReparationService();
         this.clientService = new InMemoryClientService();
+        this.empruntService = new InMemoryEmpruntService();
     }
 
     public static synchronized UiServices get() {
@@ -38,21 +42,22 @@ public final class UiServices {
         return clientService;
     }
 
-    /**
-     * Permet de remplacer le mock par une vraie impl plus tard,
-     * sans modifier les pages/controllers UI.
-     */
+    public EmpruntService emprunts() {
+        return empruntService;
+    }
+
     public synchronized void overrideReparationService(ReparationService service) {
         if (service == null) throw new IllegalArgumentException("service == null");
         this.reparationService = service;
     }
 
-    /**
-     * Permet de remplacer le mock Client par une vraie impl plus tard,
-     * sans modifier les pages/controllers UI.
-     */
     public synchronized void overrideClientService(ClientService service) {
         if (service == null) throw new IllegalArgumentException("service == null");
         this.clientService = service;
+    }
+
+    public synchronized void overrideEmpruntService(EmpruntService service) {
+        if (service == null) throw new IllegalArgumentException("service == null");
+        this.empruntService = service;
     }
 }
