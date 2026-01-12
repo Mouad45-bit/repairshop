30b12984d2eaedd6c52d4014d7000fdaebb8edc2
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-12 12:57:49 +0100

    create java/*/ui/pages/ClientsPanel.java v1

---
+++ src/main/java/com/maven/repairshop/ui/pages/ClientsPanel.java
@@ -0,0 +1,16 @@
+package com.maven.repairshop.ui.pages;
+
+import javax.swing.JPanel;
+
+public class ClientsPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public ClientsPanel() {
+
+	}
+
+}
