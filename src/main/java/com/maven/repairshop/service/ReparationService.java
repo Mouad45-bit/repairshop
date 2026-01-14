63c1e686bce7b534450628e6d5d4df12c768abde
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-14 19:55:52 +0100

    update java/*/service/ReparationService.java v2

--- src/main/java/com/maven/repairshop/service/ReparationService.java
+++ src/main/java/com/maven/repairshop/service/ReparationService.java
@@ -1,12 +1,26 @@
 package com.maven.repairshop.service;
 
+import com.maven.repairshop.model.Appareil;
 import com.maven.repairshop.model.Reparation;
 import com.maven.repairshop.model.enums.StatutReparation;
+
 import java.util.List;
 
 public interface ReparationService {
     Reparation creerReparation(Long clientId, Long reparateurId);
+    Reparation creerReparation(
+            Long clientId,
+            Long reparateurId,
+            List<Appareil> appareils,
+            String commentaireClient,
+            Double avanceOptionnelle,
+            Long userId
+    );
+    void validerReparation(Long reparationId, Long userId);
     void changerStatut(Long reparationId, StatutReparation nouveauStatut);
+    void changerStatut(Long reparationId, StatutReparation nouveauStatut, Long userId);
+    void ajouterCommentaireTechnique(Long reparationId, String texte, Long userId);
     Reparation trouverParId(Long reparationId);
+    Reparation trouverParId(Long reparationId, Long userId);
     List<Reparation> rechercher(String query, Long reparateurId, StatutReparation filtreStatut);
 }
